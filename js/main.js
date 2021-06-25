c611a8a97f56b75807dd5a0c462f2942f143bc96
Tamara-Xakep <85578429+Tamara-Xakep@users.noreply.github.com>
2021-06-25 13:04:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ function getQuantityElements(heightElement) {
 };
 function startGame() {
   start.classList.add('hide');
+  gameArea.innerHTML = '';
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -39,14 +40,20 @@ function startGame() {
     enemy.style.top = enemy.y + 'px';
     gameArea.appendChild(enemy);
   }
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = (gameArea.offsetWidth / 2 - car.offsetWidth / 2) + 'px';
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 };
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.textContent = 'SCORE: ' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -87,6 +94,13 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+    if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left && carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top) {
+      setting.start = false;
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight + 'px';
+    }
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if (item.y >= document.documentElement.clientHeight) {
